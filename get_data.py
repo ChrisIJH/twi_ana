147461490eb9f3d5f6003dd2ec1131c27b3aa6c2
Nota IM <veritasedu@gmail.com>
2015-06-18 12:22:59 -0400

    a

--- get_data.py
+++ get_data.py
@@ -25,7 +25,8 @@ tweets['county'] = map(lambda tweet: tweet['place']['country'] if tweet['place']
 
 
 tweets_by_lang = tweets['lang'].value_counts()
-
+print tweets['text']
+"""
 fig, ax = plt.subplots()
 ax.tick_params(axis='x', labelsize=15)
 ax.tick_params(axis='y', labelsize=10) 
@@ -33,3 +34,4 @@ ax.set_xlabel('Languages', fontsize=15)
 ax.set_ylabel('Number of tweets' , fontsize=15) 
 ax.set_title('Top 5 languages', fontsize=15, fontweight='bold') 
 tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+"""
